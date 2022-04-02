c6eeafd0f4a32e2282604f4d364f024cc3974227
dlarocque <larocq17@myumanitoba.ca>
2022-04-01 23:10:00 -0500

    Added JSON data initialization and retrieval

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"reflect"
 )
 
 const (
-	viewCommandName  = "view"
-	trackCommandName = "track"
-	logCommandName   = "log"
+	viewCommandName      = "view"
+	trackCommandName     = "track"
+	logCommandName       = "log"
+	jsonDataPath         = "./data/data.json"
+	jsonTemplateDataPath = "./data/template.json"
 )
+
+type Data map[string]interface{}
 
 func main() {
 	args := os.Args
@@ -71,5 +78,72 @@ func viewHabit(habitName string) {
 
 func viewAllHabits() {
 	log.Printf("Viewing all habits")
+}
 
+func getJsonData(filePath string) (Data, error) {
+	log.Printf("getting JSON data")
+	var jsonData map[string]interface{}
+	if _, err := os.Stat(filePath); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			// data does not exist in expected directory, create it
+			return initJsonData()
+		} else {
+			return nil, err
+		}
+
+	}
+
+	jsonData, err := readAndUnmarshal(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
+
+func initJsonData() (Data, error) {
+	log.Printf("Initializing JSON data")
+	jsonTemplate, err := ioutil.ReadFile(jsonTemplateDataPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ioutil.WriteFile(jsonDataPath, jsonTemplate, 0644); err != nil {
+		return nil, err
+	}
+
+	var jsonData Data
+	if err = json.Unmarshal(jsonTemplate, &jsonData); err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
+
+func readAndUnmarshal(filePath string) (Data, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonData Data
+	if err = json.Unmarshal(data, &jsonData); err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
+
+func (d Data) equalJson(other Data) bool {
+	jsonData, err := json.MarshalIndent(d, "", "    ")
+	if err != nil {
+		return false
+	}
+
+	otherJsonData, err := json.MarshalIndent(other, "", "    ")
+	if err != nil {
+		return false
+	}
+
+	return reflect.DeepEqual(jsonData, otherJsonData)
 }
