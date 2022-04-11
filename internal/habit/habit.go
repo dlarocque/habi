93093a8eb2b5119989ca20631e4a071c0cca729d
dlarocque <larocq17@myumanitoba.ca>
2022-04-11 17:54:01 -0500

    Moved habit-related functions to internal/habit package

--- internal/habit/habit.go
+++ internal/habit/habit.go
@@ -1,11 +1,9 @@
-package main
+package habit
 
 import (
-	"errors"
 	"log"
 	"time"
 
-	"github.com/dlarocque/habi/cmd"
 	"github.com/dlarocque/habi/internal/data"
 )
 
@@ -15,67 +13,36 @@ const (
 	logCommandName   = "log"
 )
 
-func main() {
-	cmd.Execute()
-}
-
-func parseArguments(args []string) error {
-	var habitName string
-	action := args[1]
+func trackHabit(habitName string) error {
+	log.Printf("Tracking habit: %s", habitName)
 	jsonData, err := data.GetJsonData(data.JsonDataPath)
 	if err != nil {
 		return err
 	}
 
-	if action == trackCommandName {
-		habitName = args[2]
-		jsonData.trackHabit(habitName) // Name collision
-	} else if action == logCommandName {
-		habitName = args[2]
-		jsonData.logHabit(habitName) // Name collision
-	} else if action == viewCommandName {
-		if len(args) == 3 {
-			habitName = args[2]
-			viewHabit(habitName)
-		} else {
-			viewAllHabits()
-		}
-	}
-
-	if err := jsonData.MarshalAndWrite(data.JsonDataPath); err != nil {
-		return err
-	}
-	return nil
-}
-
-func validateArguments(args []string) error {
-	if len(args) == 1 {
-		return errors.New("Invalid arguments")
-	}
-	return nil
-}
-
-func (d data.Data) trackHabit(habitName string) {
-	log.Printf("Tracking habit: %s", habitName)
-
 	// Don't do anything if the habit already exists
-	if _, ok := d.Habits[habitName]; ok {
+	if _, ok := jsonData.Habits[habitName]; ok {
 		log.Printf("Habit %s already exists", habitName)
-		return
+		return nil
 	}
 
 	var habit []time.Time
-	d.Habits[habitName] = habit
+	jsonData.Habits[habitName] = habit
+	return nil
 }
 
-func (d Data) logHabit(habitName string) {
+func logHabit(habitName string) error {
 	log.Printf("Logging %s", habitName)
+	jsonData, err := data.GetJsonData(data.JsonDataPath)
+	if err != nil {
+		return err
+	}
 
-	pattern, ok := d.Habits[habitName]
+	pattern, ok := jsonData.Habits[habitName]
 	if !ok {
 		log.Printf("Failed to log %s as it is not being tracked\n", habitName)
 		log.Printf("To begin tracking %s, use habi track %s", habitName, habitName)
-		return
+		return nil
 	}
 
 	todaysDate := time.Now()
@@ -88,12 +55,13 @@ func (d Data) logHabit(habitName string) {
 			(currentMonth == mostRecentMonth) &&
 			(currentDay == mostRecentDay) {
 			log.Printf("Already logged %s today!", habitName)
-			return
+			return nil
 		}
 	}
 
 	updatedPattern := append(pattern, todaysDate)
-	d.Habits[habitName] = updatedPattern
+	jsonData.Habits[habitName] = updatedPattern
+	return nil
 }
 
 func viewHabit(habitName string) {
