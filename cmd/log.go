34dc2a4aa4160fb09589ae441c0f04dee2e9dca8
dlarocque <larocq17@myumanitoba.ca>
2022-04-20 15:47:11 -0500

    Added execution of Log from CLI command

--- cmd/log.go
+++ cmd/log.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"path/filepath"
 
+	"github.com/dlarocque/habi/internal/data"
+	"github.com/dlarocque/habi/internal/habit"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +16,18 @@ var logCmd = &cobra.Command{
 	Long: `Logs that a habit has been completed, allowing users to look back at habit patterns for a habit over time.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("log called")
+		dataPath := filepath.Join(absRootPath, data.DataPath, data.JsonDataFileName)
+		data, err := data.GetJsonData(dataPath)
+		if err != nil {
+			panic(err) // TODO: Should not manually invoke a panic
+		}
+
+		if len(args) == 0 {
+			log.Fatalf("No habit name provided.")
+		}
+
+		habit.LogHabit(data, args[0])
+		data.MarshalAndWrite(dataPath)
 	},
 }
 
