a51ffc0a90c816df185ed7f896555c8863ed5f90
dlarocque <larocq17@myumanitoba.ca>
2022-04-20 14:05:51 -0500

    Added view command to CLI

---
+++ cmd/view.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// trackCmd represents the track command
+var viewCmd = &cobra.Command{
+	Use:   "view",
+	Short: "Visually displays habit patterns over time.",
+	Long: `Shows which days habits were logged since tracking began.
+Patterns can be viewed for a single habit or all habits at once.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("view called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(viewCmd)
+}
