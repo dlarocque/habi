3f105999e477454a486da3b6e52b1d83439b1cab
dlarocque <larocq17@myumanitoba.ca>
2022-04-20 14:02:34 -0500

    Removed example usage in track command

--- cmd/track.go
+++ cmd/track.go
@@ -11,9 +11,7 @@ var trackCmd = &cobra.Command{
 	Use:   "track",
 	Short: "Begin tracking a new habit",
 	Long: `Begins tracking habit patterns for a new habit, that can be viewed with the view command.
-
-Example Usage:
-$ habi track stretching`,
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("track called")
 	},
